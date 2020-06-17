111f4890ce3cd9fbe80436015da31b0d344dc8e7
Antonio Carlos <meugninuosoan@gmail.com>
2020-06-17 04:40:20 -0300

    Primeiro commit

---
+++ aula3.py
@@ -0,0 +1,45 @@
+a = int (input('primeiro bimestre: '))
+if a > 10:
+    a = int(input('Você digitou errado. Primeiro bimestre: '))
+b = int (input('segundo bimestre: '))
+if b > 10:
+    b = int(input('Você digitou errado. segundo bimestre: '))
+c = int (input('terceiro bimestre: '))
+if c > 10:
+   c = int(input('Você digitou errado. terceiro bimestre: '))
+d = int (input('quarto bimestre: '))
+if d > 10:
+    d = int(input('Você digitou errado. quarto bimestre: '))
+media = (a + b + c + d) / 4
+print('media: {}' .format(media))
+# if a <= 10 and b <= 10 and c <= 10 and d <=10:
+#     print('media: {}' .format(media))
+# else:
+#     print('digita a nota certa, animal!!')
+
+# #segunda parte
+# a = int (input('entre com um valor a: '))
+# b = int (input('entre com um valor b: '))
+# resto_a = a % 2
+# resto_b = b % 2
+#
+# # if resto_a == 0 or resto_b == 0:
+# if resto_a == 0 or not resto_b > 0:
+#     print('foi digitado numero é par')
+# else:
+#     print('não tem número é impar')
+
+
+# #primeira parte
+# a = int(input('primeiro valor: '))
+# b = int(input('segundo valor: '))
+# c = int(input('Terceiro valor: '))
+#
+# if a > b and a >c:
+#     print('o maior valor é {a}' .format(a=a))
+# elif b > a and b > c:
+#     print('o maior valor é: {}' .format(b))
+# else:
+#     print('o maior valor é: {}' .format(c))
+# print('\nfinal do programa')
+
